1405b51a6655594f86ab386a5901409f6e2705e4
Utkarsh Tiwari <utkarsh02t@gmail.com>
2023-11-03 21:26:02 +0530

    Init

---
+++ example/pqc_interop_example.dart
@@ -0,0 +1,9 @@
+import 'package:pqc_interop/pqc_interop.dart';
+
+import 'dart:ffi' as ffi;
+import 'dart:io' show Platform, Directory;
+
+import 'package:path/path.dart';
+
+typedef KyberFunc = ffi.Void Function();
+void main() {}
---
+++ test/pqc_interop_test.dart
@@ -0,0 +1,4 @@
+import 'package:pqc_interop/pqc_interop.dart';
+import 'package:test/test.dart';
+
+void main() {}
